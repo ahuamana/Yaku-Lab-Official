7d302bdc5199e0fa036706307e35405bf8d19486
ahuamana <73962071@continental.edu.pe>
2023-04-22 16:48:59 -0500

    if applied this will fix google services json

---
+++ feature-leaderboard/build/tmp/kapt3/stubs/debug/com/paparazziteam/yakulab/leaderboard/LeaderParentActivity.java
@@ -0,0 +1,27 @@
+package com.paparazziteam.yakulab.leaderboard;
+
+import java.lang.System;
+
+@kotlin.Metadata(mv = {1, 7, 1}, k = 1, d1 = {"\u0000 \n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0002\b\u0005\n\u0002\u0010\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\u0018\u00002\u00020\u0001B\u0005\u00a2\u0006\u0002\u0010\u0002J\u0012\u0010\t\u001a\u00020\n2\b\u0010\u000b\u001a\u0004\u0018\u00010\fH\u0014R\u001c\u0010\u0003\u001a\u0004\u0018\u00010\u0004X\u0086\u000e\u00a2\u0006\u000e\n\u0000\u001a\u0004\b\u0005\u0010\u0006\"\u0004\b\u0007\u0010\b\u00a8\u0006\r"}, d2 = {"Lcom/paparazziteam/yakulab/leaderboard/LeaderParentActivity;", "Landroidx/appcompat/app/AppCompatActivity;", "()V", "binding", "Lcom/paparazziteam/yakulab/leaderboard/databinding/ActivityLeaderParentBinding;", "getBinding", "()Lcom/paparazziteam/yakulab/leaderboard/databinding/ActivityLeaderParentBinding;", "setBinding", "(Lcom/paparazziteam/yakulab/leaderboard/databinding/ActivityLeaderParentBinding;)V", "onCreate", "", "savedInstanceState", "Landroid/os/Bundle;", "feature-leaderboard_debug"})
+public final class LeaderParentActivity extends androidx.appcompat.app.AppCompatActivity {
+    @org.jetbrains.annotations.Nullable
+    private com.paparazziteam.yakulab.leaderboard.databinding.ActivityLeaderParentBinding binding;
+    
+    public LeaderParentActivity() {
+        super();
+    }
+    
+    @org.jetbrains.annotations.Nullable
+    public final com.paparazziteam.yakulab.leaderboard.databinding.ActivityLeaderParentBinding getBinding() {
+        return null;
+    }
+    
+    public final void setBinding(@org.jetbrains.annotations.Nullable
+    com.paparazziteam.yakulab.leaderboard.databinding.ActivityLeaderParentBinding p0) {
+    }
+    
+    @java.lang.Override
+    protected void onCreate(@org.jetbrains.annotations.Nullable
+    android.os.Bundle savedInstanceState) {
+    }
+}
