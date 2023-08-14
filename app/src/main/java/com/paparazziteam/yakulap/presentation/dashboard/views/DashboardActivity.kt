8a1161ab0d4c7f236dba932bcb50be03624aaffc
ahuamana <73962071@continental.edu.pe>
2023-08-14 17:12:40 -0500

    if applied this will implement new menu

--- app/src/main/java/com/paparazziteam/yakulap/presentation/dashboard/views/DashboardActivity.kt
+++ app/src/main/java/com/paparazziteam/yakulap/presentation/dashboard/views/DashboardActivity.kt
@@ -1,33 +1,29 @@
 package com.paparazziteam.yakulap.presentation.dashboard.views
 
-import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
-import android.view.View
+import android.view.MenuItem
 import androidx.activity.viewModels
-import androidx.appcompat.widget.Toolbar
-import androidx.constraintlayout.widget.ConstraintLayout
-import androidx.core.content.ContextCompat
-import androidx.drawerlayout.widget.DrawerLayout
 import androidx.lifecycle.lifecycleScope
+import androidx.navigation.NavHostController
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
-import com.google.android.material.navigation.NavigationView
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.navigation.NavigationBarView
 import com.google.android.material.textview.MaterialTextView
 import com.paparazziteam.yakulab.binding.helper.application.MyPreferences
 import com.paparazziteam.yakulab.binding.helper.base.BaseActivity
-import com.paparazziteam.yakulab.binding.utils.replaceFirstCharInSequenceToUppercase
-import com.paparazziteam.yakulab.binding.utils.setColorToStatusBar
-import com.paparazziteam.yakulab.binding.utils.toast
+import com.paparazziteam.yakulab.binding.helper.replaceFirstCharInSequenceToUppercase
+import com.paparazziteam.yakulab.binding.helper.setColorToStatusBar
+import com.paparazziteam.yakulab.binding.helper.toast
 import com.paparazziteam.yakulap.R
 import com.paparazziteam.yakulap.databinding.ActivityDashboardBinding
-import com.paparazziteam.yakulap.presentation.bienvenida.views.WelcomeActivity
 import com.paparazziteam.yakulap.presentation.dashboard.fragments.BottomDialogFragment
 import com.paparazziteam.yakulap.presentation.dashboard.viewmodels.ViewModelDashboard
 import com.paparazziteam.yakulap.navigation.NavigationRootImpl
-import com.paparazziteam.yakulap.presentation.puntaje.views.PuntajeActivity
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
 import timber.log.Timber
@@ -39,10 +35,6 @@ class DashboardActivity : BaseActivity<ActivityDashboardBinding>(ActivityDashboa
 
     private lateinit var appBarConfiguration: AppBarConfiguration
 
-    private lateinit var mainToolbar: Toolbar
-    lateinit var drawer: DrawerLayout
-    lateinit var navView: NavigationView
-    lateinit var headerLayout: View
     lateinit var greetingsNameHeader: MaterialTextView
 
     @Inject
@@ -57,45 +49,25 @@ class DashboardActivity : BaseActivity<ActivityDashboardBinding>(ActivityDashboa
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        binding.apply {
-            mainToolbar = appBarDashboard.toolbar
-        }
-
         setColorToStatusBar(this)
-
-        setSupportActionBar(mainToolbar)
         saveLogin()
 
-        binding.appBarDashboard.fab.setOnClickListener { view ->
-            //Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).setAction("Action", null).show()
-            startActivity(Intent(applicationContext, PuntajeActivity::class.java))
-        }
-        drawer = binding.drawerLayout
-        navView = binding.navView
-        val navController = findNavController(R.id.nav_host_fragment_content_dashboard)
-        appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.nav_home,
-                R.id.nav_challenge_complete,
-            ), drawer
-        )
-        //Here we bind the navController to the navigationRoot
-        navigationRoot.bind(navController)
-
-        setupActionBarWithNavController(navController, appBarConfiguration)
-
-        //Change color toolbar
-        setToolbarConfig()
+        //Setup BottomNavigation
+        setupBottomNavigation()
 
         //Observers
         observers()
-
-
-        //setupDrawerItems
-        setupDrawerMenuLeft()
     }
 
-    private fun setupDrawerMenuLeft() {
+    private fun setupBottomNavigation() {
+        val navHostController = supportFragmentManager.findFragmentById(R.id.nav_host_dashboard_fragment) as NavHostFragment
+
+        binding.bottomNavView.setupWithNavController(navHostController.navController)
+    }
+
+
+    //TODO: IMPLEMENTAR PROFILE
+    /*private fun setupDrawerMenuLeft() {
         println("Header setup")
         headerLayout = navView.getHeaderView(0)
         val menuSignOut: ConstraintLayout = headerLayout.findViewById(R.id.item_menu_signout)
@@ -122,7 +94,7 @@ class DashboardActivity : BaseActivity<ActivityDashboardBinding>(ActivityDashboa
             })
         }
 
-    }
+    }*/
 
 
     private fun observers() {
@@ -156,15 +128,6 @@ class DashboardActivity : BaseActivity<ActivityDashboardBinding>(ActivityDashboa
 
     private fun saveLogin() {
         preferences.isLogin = true
-    }
-
-
-    private fun setToolbarConfig() {
-        mainToolbar.apply {
-            title = "Desafíos"
-            setTitleTextColor(ContextCompat.getColor(context, R.color.colorWhite))
-            navigationIcon = getDrawable(R.drawable.ic_menu)
-        }
     }
 
     private fun bottomDialogFragment() {
