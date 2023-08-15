0c6272767dcfa19163ea86f4823635c2ba1e795b
ahuamana <73962071@continental.edu.pe>
2023-08-14 22:05:19 -0500

    if applied this will implement new module profile

---
+++ feature-profile/src/main/java/com/yakulab/feature/profile/ProfileParentFragment.kt
@@ -0,0 +1,31 @@
+package com.yakulab.feature.profile
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.yakulab.feature.profile.databinding.FragmentProfileParentBinding
+import dagger.hilt.android.AndroidEntryPoint
+import dagger.hilt.android.HiltAndroidApp
+import viewBinding
+
+
+@AndroidEntryPoint
+class ProfileParentFragment : Fragment() {
+
+    val binding by viewBinding(FragmentProfileParentBinding::bind)
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding = FragmentProfileParentBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        //Code here
+    }
+}
---
+++ feature-profile/src/main/java/com/yakulab/feature/profile/viewmodels/ProfileParentViewModel.kt
@@ -0,0 +1,10 @@
+package com.yakulab.feature.profile.viewmodels
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class ProfileParentViewModel  @Inject constructor() : ViewModel() {
+
+}
