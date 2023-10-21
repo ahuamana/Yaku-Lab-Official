47525cf8d0816a0456109bdc807f75866bb20e52
ahuamana <73962071@continental.edu.pe>
2023-10-21 15:57:35 -0500

    if applied this will clear data from login

--- usecases/src/main/java/com/yakulab/usecases/yakulab/LogoutUseCase.kt
+++ usecases/src/main/java/com/yakulab/usecases/yakulab/LogoutUseCase.kt
@@ -1,10 +1,11 @@
 package com.yakulab.usecases.yakulab
 
 import com.ahuaman.data.dashboard.repository.LoginRepository
+import java.util.prefs.Preferences
 import javax.inject.Inject
 
 class LogoutUseCase @Inject constructor(
-    private val loginRepository: LoginRepository
+    private val loginRepository: LoginRepository,
 ) {
 
     operator fun invoke() {
