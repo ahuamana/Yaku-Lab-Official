38b7badc8cc96d0c7d0216ed5afbd06b54d39ec6
ahuamana <73962071@continental.edu.pe>
2023-10-25 16:09:56 -0500

    if applied this will add compose module for feature-splash

---
+++ feature-splash/src/main/java/com/ahuaman/feature_splash/SplashActivity.kt
@@ -0,0 +1,49 @@
+package com.ahuaman.feature_splash
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+class SplashActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            SplashContent()
+        }
+    }
+}
+
+@Composable
+fun SplashContent() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White)
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(
+                text = "Splash",
+                style = MaterialTheme.typography.bodyLarge
+            )
+        }
+    }
+}
