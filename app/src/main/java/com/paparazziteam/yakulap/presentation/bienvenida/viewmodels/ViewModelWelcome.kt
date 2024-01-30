0928b626207c70d0186e4a5b602118c9519bdf03
ahuamana <73962071@continental.edu.pe>
2024-01-30 00:13:06 -0500

    if applied this will add dialog to show disclosure message about our politics

--- app/src/main/java/com/paparazziteam/yakulap/presentation/bienvenida/viewmodels/ViewModelWelcome.kt
+++ app/src/main/java/com/paparazziteam/yakulap/presentation/bienvenida/viewmodels/ViewModelWelcome.kt
@@ -4,25 +4,47 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.paparazziteam.yakulab.binding.helper.application.MyPreferences
 import com.paparazziteam.yakulap.presentation.bienvenida.model.IntroRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import javax.inject.Inject
 
 @HiltViewModel
 class ViewModelWelcome @Inject public constructor(
-   private val repository: IntroRepository
+   private val repository: IntroRepository,
+   private val myPreferences: MyPreferences
 ): ViewModel() {
 
     private val _nextActivity = MutableLiveData<Boolean>()
     val nextActivity :LiveData<Boolean> = _nextActivity
 
+    private val _needToShowDialogDisclosure  = MutableStateFlow(true)
+    val needToShowDialogPreferences : StateFlow<Boolean> = _needToShowDialogDisclosure.asStateFlow()
+
+
     init {
         getFragments()
+        showDialogDisclosure()
+    }
+
+    private fun showDialogDisclosure() {
+        _needToShowDialogDisclosure.value = myPreferences.needsToShowTutorial
+    }
+
+
+
+    fun updateTutorialToNeverShowAgain() {
+        myPreferences.needsToShowTutorial = false
+        _needToShowDialogDisclosure.value = false
     }
 
     fun onClickStart(){
         _nextActivity.value = true
         _nextActivity.value = false
+        updateTutorialToNeverShowAgain()
     }
 
     fun getFragments(){
